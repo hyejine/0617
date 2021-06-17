02c376bedd6b9422c308dd1647c9ccd0f5b874ad
hyejine <hyejine1997@naver.com>
2021-06-17 17:55:14 +0900

    db

---
+++ server/server.js
@@ -0,0 +1,72 @@
+
+const express = require("express"); 
+// const fs = require("fs");
+const app = express();
+const port = 3005; // react의 기본값은 3000이니까 3000이 아닌 아무 수
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mysql = require("mysql"); // mysql 모듈 사용
+
+var connection = mysql.createConnection({
+    host : "localhost",
+    user : "root", //mysql의 id
+    password : "0000", //mysql의 password
+    database : "chart", //사용할 데이터베이스
+});
+
+connection.connect();    //DB연동
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+// const data =fs.readFileSync('./database.json');
+// const conf = JSON.parse(data);
+
+app.get('/', (req, res) =>{
+    res.send(' Hello world')  
+})  
+
+// 데이터 삽입 
+app.post("/idplz", (req,res)=>{
+    const test = req.body.test;    
+    // body.test 는 어디서 온건지?? 문법 인가 ? => oo
+    // req.body.test;  여기에 데이터를 넣어 보여준다. 
+    // 
+    console.log(test);
+
+    connection.query("INSERT INTO test (test) values(?)",[test],
+    function(err,rows,fields){
+        if(err){
+            console.log("실패");
+            console.log(err);
+        }else{
+            console.log("성공");
+            console.log(rows);
+        };
+    });
+}); 
+
+
+app.post("/callbody", (req,res)=>{
+    connection.query("SELECT * FROM test",
+    function(err,rows,fields){
+        if(err){
+            console.log("불러오기 실패");
+            console.log(err);
+        }else{
+            console.log("불러오기 성공");
+            res.send(rows)
+            
+            
+            // let i;
+            // for(i =0; i<rows.lenght; i++){
+            // res.send(rows[i]);
+            // }console.log(rows[i].COLUMN_NAME);
+        }
+    })
+})
+
+app.listen(port, ()=>{
+    console.log(`Connect at http://localhost:${port}`);
+})
---
+++ src/Example3.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react'
+import './App.css';
+export default class App extends Component {
+  state = {
+    testbody : "",
+    data : "",
+  }
+
+  handleChange =(e)=>{
+    this.setState({
+      [e.target.name] : e.target.value,
+    });
+  }
+
+  submitId = (value)=>{
+    const post ={
+      test : this.state.value,
+    };
+   //{ test: '7' ,'','','','',''}
+    fetch("http://localhost:3005/idplz", {
+      method : "post", // 통신방법
+      headers : {
+        "content-type" : "application/json",
+      },
+      body : JSON.stringify(post),
+    })
+    .then((res)=>res.json())
+    .then((json)=>{
+      this.setState({
+        testbody : json.text,
+        
+      }); 
+    });
+  };
+  onCall =()=>{
+    fetch("http://localhost:3005/callbody",{
+      method:"post",
+      headers : {
+        "content-type" : "application/json",
+      },
+      body : JSON.stringify(),
+    })
+    
+    .then((res)=>res.json())
+    .then((json)=>{
+      this.setState({
+        data : json.이름
+        
+      }); 
+    });console.log(this.state)
+    
+  }; 
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.submitId}>
+        <input onChange={this.handleChange} name ="testbody"/>
+        <input onChange={this.handleChange} name ="testbody2"/>
+        <input onChange={this.handleChange} name ="testbody"/>
+        <input onChange={this.handleChange} name ="testbody"/>
+        <input onChange={this.handleChange} name ="testbody"/>
+        <input onChange={this.handleChange} name ="testbody"/>
+        <input onChange={this.handleChange} name ="testbody"/>
+
+        <button type="submit">Submit</button>
+        </form>
+        <h1>{this.state.testbody}</h1>
+        <br/><br/><br/><br/><br/>
+        <h2>데이터가져오기</h2>
+        <h3>{this.state.data}</h3>
+        <button onClick={this.onCall}>가져오기</button>
+      </div>
+    )
+  }
+}
