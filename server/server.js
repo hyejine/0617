308889a74c3779376f40c75dda8012d07812383e
hyejine <hyejine1997@naver.com>
2021-06-18 10:55:05 +0900

    0618오전 셀렉트문에 코드 넣어야함

--- server/server.js
+++ server/server.js
@@ -1,41 +1,35 @@
-
 const express = require("express"); 
-// const fs = require("fs");
 const app = express();
 const port = 3005; // react의 기본값은 3000이니까 3000이 아닌 아무 수
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const mysql = require("mysql"); // mysql 모듈 사용
 
+
 var connection = mysql.createConnection({
     host : "localhost",
-    user : "root", //mysql의 
+    user : "root", //mysql의 id
     password : "0000", //mysql의 password
     database : "chart", //사용할 데이터베이스
 });
-
 connection.connect();    //DB연동
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
-
 // const data =fs.readFileSync('./database.json');
 // const conf = JSON.parse(data);
 
 app.get('/', (req, res) =>{
     res.send(' Hello world')  
 })  
-
 // 데이터 삽입 
 app.post("/idplz", (req,res)=>{
     const test = req.body.test;    
     // body.test 는 어디서 온건지?? 문법 인가 ? => oo
     // req.body.test;  여기에 데이터를 넣어 보여준다. 
-    // 
-    console.log(test);
 
-    connection.query("INSERT INTO test (test) values(?)",[test],
+    console.log(req.body);
+    connection.query("INSERT INTO test (이름) values('?')",[test],
     function(err,rows,fields){
         if(err){
             console.log("실패");
@@ -46,27 +40,38 @@ app.post("/idplz", (req,res)=>{
         };
     });
 }); 
-
-
 app.post("/callbody", (req,res)=>{
-    connection.query("SELECT * FROM test",
+    connection.query(
+        // "SELECT * FROM test",
+    "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'test'",
     function(err,rows,fields){
         if(err){
             console.log("불러오기 실패");
             console.log(err);
+            
         }else{
             console.log("불러오기 성공");
-            res.send(rows)
+            // console.log(rows[1].COLUMN_NAME);
+            // console.log(rows[2]);
+            // console.log(rows);
+
+            let i;
+            console.log("sad",rows.length);
+            for (i=0; i<rows.length; i++){
+                console.log(rows[i].COLUMN_NAME);
+                res.send(rows[i].COLUMN_NAME);
+
+            }
+
             
+
+            // 왜 홍길동 밖에 안나왔던건지 모르겠음 
+            // => json 에서 이름만 가져왔었음 
+            // res.send(rows[2])
             
-            // let i;
-            // for(i =0; i<rows.lenght; i++){
-            // res.send(rows[i]);
-            // }console.log(rows[i].COLUMN_NAME);
         }
     })
 })
-
 app.listen(port, ()=>{
     console.log(`Connect at http://localhost:${port}`);
 })
--- src/Example3.js
+++ src/Example3.js
@@ -5,18 +5,22 @@ export default class App extends Component {
     testbody : "",
     data : "",
   }
+  //state?? 
 
   handleChange =(e)=>{
-    this.setState({        //dd
+    this.setState({       
       [e.target.name] : e.target.value,
     });
+    // 새로운 값 부여? target.name?? 
   }
 
   submitId = (value)=>{
     const post ={
       test : this.state.value,
     };
-   //{ test: '7' ,'','','','',''}
+   // { test: '7' ,'','','','',''}
+
+  // submitId 함수안에 fetch함수를 작성하여 server에 보낼 준비한다.
     fetch("http://localhost:3005/idplz", {
       method : "post", // 통신방법
       headers : {
@@ -33,6 +37,7 @@ export default class App extends Component {
     });
   };
   onCall =()=>{
+    // fetch는 url주소는 우리가 데이터를 보낼 주소
     fetch("http://localhost:3005/callbody",{
       method:"post",
       headers : {
@@ -44,30 +49,35 @@ export default class App extends Component {
     .then((res)=>res.json())
     .then((json)=>{
       this.setState({
-        data : json.이름
+        // 가져 오고 싶은 데이터 뽑기 
+        data : json.이름,
+        data2 : json.성별,
+        data3 : json.COLUMN_NAME
         
       }); 
     });console.log(this.state)
-    
+     // res로 받은것을 json으로 변환하고 json.text을 this.state.id에 저장한다.
   }; 
   render() {
     return (
       <div>
         <form onSubmit={this.submitId}>
         <input onChange={this.handleChange} name ="testbody"/>
-        <input onChange={this.handleChange} name ="testbody2"/>
+        {/* <input onChange={this.handleChange} name ="testbody2"/>
         <input onChange={this.handleChange} name ="testbody"/>
         <input onChange={this.handleChange} name ="testbody"/>
         <input onChange={this.handleChange} name ="testbody"/>
         <input onChange={this.handleChange} name ="testbody"/>
-        <input onChange={this.handleChange} name ="testbody"/>
-
+        <input onChange={this.handleChange} name ="testbody"/> */}
         <button type="submit">Submit</button>
         </form>
         <h1>{this.state.testbody}</h1>
         <br/><br/><br/><br/><br/>
         <h2>데이터가져오기</h2>
         <h3>{this.state.data}</h3>
+        <h3>{this.state.data2}</h3>
+        <h3>{this.state.data3}</h3>
+
         <button onClick={this.onCall}>가져오기</button>
       </div>
     )
